8842a046c97ccbb2276de96756d5e46e11335c94
mrunalimohite <103097897+mrunalimohite@users.noreply.github.com>
2025-08-04 21:44:50 +0530

    Add files via upload

--- internship-task/index.php
+++ internship-task/index.php
@@ -42,7 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login/Signup Page</title>
+    <title>Login Page</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body class="login-page">
